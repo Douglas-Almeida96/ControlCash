8e20dafcac498a4acae9c9ecb81f5cce1be277b1
Douglas Almeida <douglas.almeida377@gmail.com>
2020-07-30 20:19:47 -0300

    subindo projeto asp.net

---
+++ WebApplication3/Migrations/20200730224753_addagendaBarber.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Data.Migrations
+{
+    public partial class addagendaBarber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agenda_Barbeiros",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TempoDeProdução = table.Column<int>(nullable: false),
+                    Domingo = table.Column<bool>(nullable: false),
+                    Segunda = table.Column<bool>(nullable: false),
+                    Terca = table.Column<bool>(nullable: false),
+                    Quarta = table.Column<bool>(nullable: false),
+                    Quinta = table.Column<bool>(nullable: false),
+                    Sexta = table.Column<bool>(nullable: false),
+                    Sabado = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agenda_Barbeiros", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agenda_Barbeiros");
+        }
+    }
+}
---
+++ WebApplication3/model/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.model
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Agenda_barbeiro> Agenda_Barbeiros{ get; set; }
+    }
+}
